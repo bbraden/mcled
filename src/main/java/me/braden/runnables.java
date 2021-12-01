8e1d43a07384352d29341668083e243a6c5b98a3
braden <73503391+bbraden@users.noreply.github.com>
2021-12-01 00:04:33 -0600

    updates

--- src/main/java/me/braden/runnables.java
+++ src/main/java/me/braden/runnables.java
@@ -1,9 +1,18 @@
 package me.braden;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.*;
 import jssc.SerialPortException;
+import org.bson.Document;
 import org.bukkit.scheduler.BukkitRunnable;
-import static me.braden.methods.turnBlue;
-import static me.braden.methods.turnGreen;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static com.mongodb.client.model.Filters.eq;
+import static me.braden.methods.*;
 import static me.braden.variables.*;
 
 public class runnables {
@@ -39,5 +48,55 @@ public class runnables {
             countdown--;
 
         }
-}
+    }
+    public static class mongo extends BukkitRunnable {
+
+        @Override
+        public void run() {
+            List<Document> inserts = new ArrayList<>();
+            ConnectionString connectionString = new ConnectionString("mongodb+srv://braden:1234@cluster0.w4snx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .build();
+            MongoClient mongoClient = MongoClients.create(settings);
+            MongoDatabase database = mongoClient.getDatabase("chat");
+            MongoCollection<Document> collection = database.getCollection("chat");
+            FindIterable<Document> findIterable = collection.find(new Document());
+            Document myDoc = collection.find(eq("message", "no ur sus")).first();
+            String json = myDoc.toJson();
+            FindIterable<Document> iterDoc = collection.find();
+            while (true) {
+                for (Document document : iterDoc) {
+                    if (document == null) {
+                        return;
+                    } else {
+                        if (!inserts.contains(document)) {
+                            inserts.add(document);
+                            System.out.println(document.get("message").toString());
+                            recentEntry = document.get("message").toString();
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    public static class mongoChecker extends BukkitRunnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("tried");
+                if (Objects.equals(recentEntry, "red")) {
+                    try {
+                        turnRed(com);
+                    } catch (SerialPortException e) {
+                        e.printStackTrace();
+                    }
+            }
+            }
+        }
+    }
 }
